7e637d23913b279e543b4637eb174bffdbe3acb5
lhutnik <picoproject9@gmail.com>
2025-08-20 11:27:21 -0600

    Implementing GDL/IDL output integration

---
+++ gravitational-scattering/maketxt.pro
@@ -0,0 +1,29 @@
+pro maketxt
+
+pc_read_pstalk,obj=fs
+
+;xp yp zp vpx vpy vpz rhopswarm aps ux uy uz rho potself                                                                                                                                                                                 
+print, tag_names(fs) ; Print all tags of read in object 
+
+tmp = size(fs.t)    
+nt=tmp[1]            ; Number of times stalked
+tmp = size(fs.ipar) 
+npar=tmp[1]          ; Number of particles stalked
+
+for it=1,nt-1 do begin ; For all timesteps
+   fname='file_'+string(it,format="(i04)")+'.txt' ; Save printf to this file
+   filepath='~/pencil-kbobinary/gravitational-scattering/output/'+fname
+   openw,lun,filepath,/get_lun,/append
+
+   printf,lun,'# Timestep=',it," Time=",fs.t[it],format='%s %i %s %f'
+   printf,lun,'# particle, x, y, rhopswarm, aps'
+   for k=0,npar-1 do begin ; For all particles  
+      if (fs.aps[k,it] ne 0) then begin
+         printf,lun,fs.ipar[k],fs.xp[k,it],fs.yp[k,it],fs.rhopswarm[k,it],fs.aps[k,it],format='%i %f %f %f %f'
+      endif
+   endfor
+   close,lun
+   free_lun,lun
+endfor
+
+end
--- gravitational-scattering/point-plot.py
+++ gravitational-scattering/point-plot.py
@@ -9,30 +9,46 @@ import matplotlib.pyplot as plt # Plotting
 #xp1, yp1 = ts.xpm, ts.ypm             # Setting x and y variables
 
 ## Input or generate array with particle information
-if os.path.exists(os.path.join('.','outparray.csv')):       # If a .csv is already available, do not reproduce
-    parray = np.loadtxt('outparray.csv', delimiter=',')     # Reuse previously made .csv from relevant data
-else: 
-    SIM = pc.get_sim()                                      # Pull simulation 
-    varfiles = 'all' #'VAR###', 'last4', 'first3'           # Specifying which VAR/PVAR files to pull
-    varlist = SIM.get_varlist(pos=varfiles, particle=False) # Pull time and cell information from VAR files
-    pvarlist = SIM.get_varlist(pos=varfiles, particle=True) # Pull particle information from PVAR files
+#if os.path.exists(os.path.join('.','outparray.csv')):       # If a .csv is already available, do not reproduce
+#    parray = np.loadtxt('outparray.csv', delimiter=',')     # Reuse previously made .csv from relevant data
+#else: 
+#    SIM = pc.get_sim()                                      # Pull simulation 
+#    varfiles = 'all' #'VAR###', 'last4', 'first3'           # Specifying which VAR/PVAR files to pull
+#    varlist = SIM.get_varlist(pos=varfiles, particle=False) # Pull time and cell information from VAR files
+#    pvarlist = SIM.get_varlist(pos=varfiles, particle=True) # Pull particle information from PVAR files
+#
+#    parray = np.empty(shape=(len(pvarlist), 11))             # Empty array to insert data into
+#    for i, (f, p) in enumerate(zip(varlist, pvarlist)):
+#        ff = pc.read.var(datadir=SIM.datadir, var_file=f, quiet=True, trimall=False)
+#        pp = pc.read.pvar(datadir=SIM.datadir, pvarfile=p, quiet=True)
+#        #l_ipars = pp.ipars # Particle number
+#        l_px = pp.xp    # Particle x position                                 
+#        l_py = pp.yp    # Particle y position 
+#        #l_pz = pp.zp   # Particle z position 
+#        l_vx = pp.vpx   # Particle x velocity 
+#        l_vy = pp.vpy   # Particle y velocity 
+#        #l_vz = pp.vpz  # Particle z velocity
+#        l_aps = pp.aps  # Particle radii   
+#        t = ff.t
+#        parray[i] = [t, l_px[0], l_py[0], l_vx[0], l_vy[0], l_px[1], l_py[1], l_vx[1], l_vy[1], l_aps[0], l_aps[1]]
+#    print(parray)
+#    np.savetxt('outparray.csv', parray, delimiter=',', header='t, l_px[0], l_py[0], l_vx[0], l_vy[0], l_px[1], l_py[1], l_vx[1], l_vy[1], l_aps[0], l_aps[1]')
 
-    parray = np.empty(shape=(len(pvarlist), 11))             # Empty array to insert data into
-    for i, (f, p) in enumerate(zip(varlist, pvarlist)):
-        ff = pc.read.var(datadir=SIM.datadir, var_file=f, quiet=True, trimall=False)
-        pp = pc.read.pvar(datadir=SIM.datadir, pvarfile=p, quiet=True)
-        #l_ipars = pp.ipars # Particle number
-        l_px = pp.xp    # Particle x position                                 
-        l_py = pp.yp    # Particle y position 
-        #l_pz = pp.zp   # Particle z position 
-        l_vx = pp.vpx   # Particle x velocity 
-        l_vy = pp.vpy   # Particle y velocity 
-        #l_vz = pp.vpz  # Particle z velocity
-        l_aps = pp.aps  # Particle radii   
-        t = ff.t
-        parray[i] = [t, l_px[0], l_py[0], l_vx[0], l_vy[0], l_px[1], l_py[1], l_vx[1], l_vy[1], l_aps[0], l_aps[1]]
-    print(parray)
-    np.savetxt('outparray.csv', parray, delimiter=',', header='t, l_px[0], l_py[0], l_vx[0], l_vy[0], l_px[1], l_py[1], l_vx[1], l_vy[1], l_aps[0], l_aps[1]')
+
+timelen = np.loadtxt(r'/home/vboxuser/pencil-kbobinary/gravitational-scattering/data/tstalk.dat')
+print(timelen)
+nstalk  = int(timelen[1]) # Second element of timelen provides number of pstalk timesteps recorded
+header  = np.loadtxt(r'/home/vboxuser/pencil-kbobinary/gravitational-scattering/output/file_0001.txt')
+print(header)
+header_len = len(header)  # Second element of .shape array provides number of columns in each pstalk timestep
+
+## Create new array to input particle 1 data
+parray1 = np.empty(shape=(nstalk, header_len))
+for i in range(0, nstalk-1): 
+    num = '{0:04}'.format(i+1) # Format each output file call e.g. 1 -> '0001'
+    output = np.loadtxt(r'/home/vboxuser/pencil-kbobinary/gravitational-scattering/output/file_'+num+'.txt')
+    parray1[i,:] = output # First row elements are substituted into the larger array
+print(parray1)
 
 ## Check the grid size to apply to the plot
 grid   = pc.read.grid()         # Read relevant information from grid data
@@ -41,14 +57,14 @@ x2, y2 = grid.x[-4], grid.y[-4] # second corner coordinates
 
 ## Plot location of particles throughout simulation
 fig, ax = plt.subplots(1, 1)                 # Figure and axes established
-ax.plot(parray[:,1], parray[:,2], 'r.', label='aps='+str(parray[-1,9]))    # Plotting particle 1 position
-ax.plot(parray[:,5], parray[:,6], 'b.', label='aps='+str(parray[-1,10]))   # Plotting particle 2 position
+ax.plot(parray1[:,1], parray1[:,2], 'r.', label='aps='+str(parray1[0,4]))    # Plotting particle 1 position
+#ax.plot(parray[:,5], parray[:,6], 'b.', label='aps='+str(parray[-1,10]))   # Plotting particle 2 position
 ax.set_xlabel(r'$x$', fontsize=15)           # x-axis label
 ax.set_ylabel(r'$y$', fontsize=15)           # y-axis label
 plt.axis('scaled')                           # Axes are scaled to match one another
 ax.set_xlim(x1, x2)                          # Set x-axis limits
 ax.set_ylim(y1, y2)                          # Set y-axis limits
-ax.set_title(r'$t={}$'.format(parray[-1,0])) # Plot title
+#ax.set_title(r'$t={}$'.format(parray1[-1,0])) # Plot title
 plt.legend()                                 # Add legend
 plt.tight_layout()                           # Remove overlapping and clipping
 plt.savefig('point-plot.pdf', dpi=300)       # Save figure as a PDF file with set quality
