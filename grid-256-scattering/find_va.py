198e7f66dbc2f9b19797c7792e08d4e47ed0c18b
Leopold Hutnik <lhutnik@astronomy.nmsu.edu>
2025-10-01 01:17:57 -0600

    256x256 grid points case added

---
+++ grid-256-scattering/find_va.py
@@ -0,0 +1,54 @@
+## Import critical libraries
+import numpy as np
+
+## Determine distance, velocity, and angle at periapsis
+vp_array = np.loadtxt("parray2.csv", delimiter=',')       # Load array of positions and velocities over time
+vp = 0                                                    # Minimum 'magnitude' of position vector
+for i in range(len(vp_array[:,0])):                       # For all time indices
+    line_m = np.sqrt(vp_array[i,4]**2 + vp_array[i,5]**2) # Velocity vector magnitude 
+    if line_m > vp:                                       # If higher, then this could be the periapsis 
+        vp = line_m                                       # Record new highest velocity
+        x_coord = vp_array[i,2]                           # x distance
+        y_coord = vp_array[i,3]                           # y distance
+        rp = np.sqrt(x_coord**2 + y_coord**2)             # 
+
+angle = np.arctan(y_coord/x_coord)                        # tan(theta)=y/x=opp/adj
+
+print('Maximum velocity:',vp)
+print('Closest distance:',rp,'(Components: x=',x_coord,'y=',y_coord,')')
+print('Origin to periapsis angle:',angle,'radians')
+
+## From quantities in code units, determine characteristics of hyperbolic trajectory
+rhopswarm   = 64.0 # Density set for sink particle
+cell_side   = 256  # Grid cells per side
+side_length = 4.0  # Length of one side
+
+G = 1/(4*np.pi)                              # Gravitational constant #0.01061032953945969
+M = (rhopswarm) * (side_length/cell_side)**2 # Mass of particle at origin
+
+a = ((2/rp)-(vp**2)/(G*M))**(-1) # Semi-major axis in code length units
+e = 1 - rp/a                     # Eccentricity
+print('A representative hyperbolic trajectory has:')
+print('Semi-major axis:',a,'code length units')
+print('Eccentricity:',e)
+
+## Alternatively, use initial conditions and updated mass
+xp = 1.0
+yp = 1.99
+vx = 0.0
+vy = -0.05
+
+M = (rhopswarm) * (side_length/cell_side)**2 # Column density is being used for 2D case?
+mu = G*M                   # Gravitational parameter
+r = np.sqrt(xp**2 + yp**2) # Initial radial distance
+v2 = vx**2 + vy**2         # Initial velocity^2 magnitude
+a = 1/(2/r - v2/mu)        # Semi-major axis from rewritten vis-viva equation
+vinf = np.sqrt(-mu/a)      # Set r=infinity for velocity at infinity
+h = abs(vy*xp - vx*yp)     # Specific angular momentum at any point in trajectory
+b = h/vinf                 # Impact parameter or semi-minor axis
+e = np.sqrt(1+b**2/a**2)   # Eccentricity from semi-minor and semi-major axes
+
+print('A representative hyperbolic trajectory for a 2D mass:')
+print('Semi-major axis:',a,'code length units')
+print('Eccentricity:',e)
+
---
+++ grid-64-scattering/maketxt.pro
@@ -0,0 +1,47 @@
+pro maketxt
+
+pc_read_pstalk,obj=fs
+; Tag names are written as the following:
+; xp yp zp vpx vpy vpz rhopswarm aps ux uy uz rho potself                                                                                                                                                                                 
+print, tag_names(fs) ; Print all tags of read in object 
+
+tmp = size(fs.t)    
+nt=tmp[1]            ; Number of times stalked
+print, tmp[0]
+
+tmp = size(fs.ipar) 
+npar=tmp[1]          ; Number of particles stalked
+print, tmp[0]
+
+print, nt
+print, npar
+
+print, "The first ipar column is:"
+print, fs.ipar[*,0,0]
+
+print, "The sizes of ipar, xp, and aps arrays are the following:"
+print, size(fs.ipar)
+print, size(fs.xp)
+print, size(fs.aps)
+
+;help, fs.xp
+print, fs.xp[*,0,0] ; xp data for first particle?
+;print, fs.xp[0,0,*]
+
+for it=0,nt-1 do begin ; For all times stalked
+   fname='file_'+string(it,format="(i04)")+'.txt' ; Save printf to this file
+   filepath='~/pencil-kbobinary/grid-64-scattering/output/'+fname
+   openw,lun,filepath,/get_lun,/append
+
+   printf,lun,'# Timestep=',it," Time=",fs.t[it],format='%s %i %s %f'
+   printf,lun,'# time, particle index, x, y, vx, vy, rhopswarm, aps'
+   for k=0,npar-1 do begin ; For all particles  
+         ;if (fs.aps[k,it] ne 0) then begin
+      printf,lun,fs.t[it],fs.ipar[k],fs.xp[k,it],fs.yp[k,it],fs.vpx[k,it],fs.vpy[k,it],fs.rhopswarm[k,it],fs.aps[k,it],format='%f %i %f %f %f %f %f %f'
+         ;endif
+   endfor
+   close,lun
+   free_lun,lun
+endfor
+
+end
